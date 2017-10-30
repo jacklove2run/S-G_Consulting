56b94d70e1c6f6a5e6075f1b6eea25353b1393ee
jacklove2run <529842912@qq.com>
2017-10-30 22:49:54 +0800

    修改获取待支付订单页面

--- testdb/testdb/Ylk/ylawyer/dbcontroller.py
+++ testdb/testdb/Ylk/ylawyer/dbcontroller.py
@@ -116,8 +116,9 @@ def response_success_order_set_json(data):
             productDesc = cur_data.product_desc
             imgUrl = cur_data.img_url
             out_trade_no = cur_data.out_trade_no
+            out_trade_show_no = cur_data.out_trade_show_no
             orderInfo = dict()
-            orderInfo.update(id=id, user_id=userId, product_name=productName, product_desc=productDesc, product_price=productPrice, time=time, img_url=imgUrl, out_trade_no=out_trade_no)
+            orderInfo.update(id=id, user_id=userId, product_name=productName, product_desc=productDesc, product_price=productPrice, time=time, img_url=imgUrl, out_trade_no=out_trade_no, out_trade_show_no=out_trade_show_no)
             #print(orderInfo)
             dataList.append(orderInfo)
         success_json['data'] = dataList
