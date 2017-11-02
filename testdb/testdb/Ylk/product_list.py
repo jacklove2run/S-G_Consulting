f741d1002e131e99866ad8f94a98e8d514277f87
jacklove2run <529842912@qq.com>
2017-11-03 04:04:30 +0800

    修改产品信息

--- testdb/testdb/Ylk/product_list.py
+++ testdb/testdb/Ylk/product_list.py
@@ -16,217 +16,217 @@ PRODUCT_LIST = [
         {
             'tabId' : '1', 
             'name' : '常用推荐', 
-            'tab_img_url' : 'https://o3pvuu23u.qnssl.com/img/lzwai.jpeg',
+            'tab_img_url' : 'http://120.25.93.29/static/1.jpg',
             'services' : 
             [
                 {
                     'product_id' : '1', 
-                    'product_name' : '合同服务', 
+                    'product_name' : '写合同', 
                     'product_price' : '500', 
                     'product_desc' : '职业律师代写', 
-                    'product_img_url' : 'https://o3pvuu23u.qnssl.com/img/lzwai.jpeg'
+                    'product_img_url' : 'http://120.25.93.29/static/1.jpg'
                 }, 
                 {
                     'product_id' : '2', 
-                    'product_name' : '合同审查', 
+                    'product_name' : '审合同', 
                     'product_price' : '300', 
                     'product_desc' : '细致入微，不会放过一个细节', 
-                    'product_img_url' : 'https://o3pvuu23u.qnssl.com/img/lzwai.jpeg'
+                    'product_img_url' : 'http://120.25.93.29/static/2.jpg'
                 }
             ]
         },
         {
             'tabId' : '2', 
             'name' : '合同服务', 
-            'tab_img_url' : 'https://o3pvuu23u.qnssl.com/img/lzwai.jpeg',
+            'tab_img_url' : 'http://120.25.93.29/static/1.jpg',
             'services' : 
             [
                 {
-                    'product_id' : '3', 
+                    'product_id' : '1', 
                     'product_name' : '写合同', 
                     'product_price' : '500.0', 
                     'product_desc' : '职业律师代写', 
-                    'product_img_url' : 'https://o3pvuu23u.qnssl.com/img/lzwai.jpeg'
+                    'product_img_url' : 'http://120.25.93.29/static/1.jpg'
                 }, 
                 {
-                    'product_id' : '4', 
+                    'product_id' : '2', 
                     'product_name' : '审合同', 
                     'product_price' : '2000.0', 
                     'product_desc' : '细致入微，不会放过一个细节', 
-                    'product_img_url' : 'https://o3pvuu23u.qnssl.com/img/lzwai.jpeg'
+                    'product_img_url' : 'http://120.25.93.29/static/2.jpg'
                 },
                 {
-                    'product_id' : '5', 
+                    'product_id' : '3', 
                     'product_name' : '写诉状', 
                     'product_price' : '2000.0', 
                     'product_desc' : '细致入微，不会放过一个细节', 
-                    'product_img_url' : 'https://o3pvuu23u.qnssl.com/img/lzwai.jpeg'
+                    'product_img_url' : 'http://120.25.93.29/static/3.jpg'
                 },
                 {
-                    'product_id' : '6', 
+                    'product_id' : '4', 
                     'product_name' : '写答辩状', 
                     'product_price' : '5000.0', 
                     'product_desc' : '细致入微，不会放过一个细节', 
-                    'product_img_url' : 'https://o3pvuu23u.qnssl.com/img/lzwai.jpeg'
+                    'product_img_url' : 'http://120.25.93.29/static/4.jpg'
                 },
                 {
-                    'product_id' : '7', 
+                    'product_id' : '5', 
                     'product_name' : '写劳动仲裁申请书', 
                     'product_price' : '5000.0', 
                     'product_desc' : '细致入微，不会放过一个细节', 
-                    'product_img_url' : 'https://o3pvuu23u.qnssl.com/img/lzwai.jpeg'
+                    'product_img_url' : 'http://120.25.93.29/static/5.jpg'
                 }
             ]
         },
         {
             'tabId' : '3', 
             'name' : '谈判服务', 
-            'tab_img_url' : 'https://o3pvuu23u.qnssl.com/img/lzwai.jpeg',
+            'tab_img_url' : 'http://120.25.93.29/static/1.jpg',
             'services' : 
             [
                 {
-                    'product_id' : '8', 
+                    'product_id' : '6', 
                     'product_name' : '个人谈判', 
                     'product_price' : '1000.0', 
                     'product_desc' : '职业律师谈判服务', 
-                    'product_img_url' : 'https://o3pvuu23u.qnssl.com/img/lzwai.jpeg'
+                    'product_img_url' : 'http://120.25.93.29/static/6.jpg'
                 }, 
                 {
-                    'product_id' : '9', 
+                    'product_id' : '7', 
                     'product_name' : '公司谈判', 
                     'product_price' : '2000', 
                     'product_desc' : '细致入微，不会放过一个细节', 
-                    'product_img_url' : 'https://o3pvuu23u.qnssl.com/img/lzwai.jpeg'
+                    'product_img_url' : 'http://120.25.93.29/static/7.jpg'
                 }
             ]
         },
         {
             'tabId' : '4', 
             'name' : '诉讼服务', 
-            'tab_img_url' : 'https://o3pvuu23u.qnssl.com/img/lzwai.jpeg',
+            'tab_img_url' : 'http://120.25.93.29/static/1.jpg',
             'services' : 
             [
                 {
-                    'product_id' : '10', 
+                    'product_id' : '8', 
                     'product_name' : '指导打官司', 
                     'product_price' : '2000.0', 
                     'product_desc' : '职业律师诉讼服务', 
-                    'product_img_url' : 'https://o3pvuu23u.qnssl.com/img/lzwai.jpeg'
+                    'product_img_url' : 'http://120.25.93.29/static/8.jpg'
                 }, 
                 {
-                    'product_id' : '11', 
+                    'product_id' : '9', 
                     'product_name' : '代理打官司', 
                     'product_price' : '5000.0', 
                     'product_desc' : '细致入微，不会放过一个细节', 
-                    'product_img_url' : 'https://o3pvuu23u.qnssl.com/img/lzwai.jpeg'
+                    'product_img_url' : 'http://120.25.93.29/static/9.jpg'
                 }
             ]
         },
         {
             'tabId' : '5', 
             'name' : '个人代办', 
-            'tab_img_url' : 'https://o3pvuu23u.qnssl.com/img/lzwai.jpeg',
+            'tab_img_url' : 'http://120.25.93.29/static/1.jpg',
             'services' : 
             [
                 {
-                    'product_id' : '12', 
+                    'product_id' : '10', 
                     'product_name' : '代办遗嘱', 
                     'product_price' : '2000.0', 
                     'product_desc' : '职业律师诉讼服务', 
-                    'product_img_url' : 'https://o3pvuu23u.qnssl.com/img/lzwai.jpeg'
+                    'product_img_url' : 'http://120.25.93.29/static/10.jpg'
                 }, 
                 {
-                    'product_id' : '13', 
+                    'product_id' : '11', 
                     'product_name' : '代办公证', 
                     'product_price' : '2000.0', 
                     'product_desc' : '细致入微，不会放过一个细节', 
-                    'product_img_url' : 'https://o3pvuu23u.qnssl.com/img/lzwai.jpeg'
+                    'product_img_url' : 'http://120.25.93.29/static/11.jpg'
                 },
                 {
-                    'product_id' : '14', 
+                    'product_id' : '12', 
                     'product_name' : '代办社保', 
                     'product_price' : '1000.0', 
                     'product_desc' : '细致入微，不会放过一个细节', 
-                    'product_img_url' : 'https://o3pvuu23u.qnssl.com/img/lzwai.jpeg'
+                    'product_img_url' : 'http://120.25.93.29/static/12.jpg'
                 }
             ]
         },
         {
             'tabId' : '6', 
             'name' : '公司代办', 
-            'tab_img_url' : 'https://o3pvuu23u.qnssl.com/img/lzwai.jpeg',
+            'tab_img_url' : 'http://120.25.93.29/static/1.jpg',
             'services' : 
             [
                 {
-                    'product_id' : '15', 
+                    'product_id' : '13', 
                     'product_name' : '公司注册', 
                     'product_price' : '2000.0', 
                     'product_desc' : '职业律师诉讼服务', 
-                    'product_img_url' : 'https://o3pvuu23u.qnssl.com/img/lzwai.jpeg'
+                    'product_img_url' : 'http://120.25.93.29/static/13.jpg'
                 }, 
                 {
-                    'product_id' : '16', 
+                    'product_id' : '14', 
                     'product_name' : '商标注册', 
                     'product_price' : '2000.0', 
                     'product_desc' : '细致入微，不会放过一个细节', 
-                    'product_img_url' : 'https://o3pvuu23u.qnssl.com/img/lzwai.jpeg'
+                    'product_img_url' : 'http://120.25.93.29/static/14.jpg'
                 },
                 {
-                    'product_id' : '17', 
+                    'product_id' : '15', 
                     'product_name' : '内部制度建设', 
                     'product_price' : '1000.0', 
                     'product_desc' : '细致入微，不会放过一个细节', 
-                    'product_img_url' : 'https://o3pvuu23u.qnssl.com/img/lzwai.jpeg'
+                    'product_img_url' : 'http://120.25.93.29/static/15.jpg'
                 },
                 {
-                    'product_id' : '18', 
+                    'product_id' : '16', 
                     'product_name' : '专项法律培训', 
                     'product_price' : '1000.0', 
                     'product_desc' : '细致入微，不会放过一个细节', 
-                    'product_img_url' : 'https://o3pvuu23u.qnssl.com/img/lzwai.jpeg'
+                    'product_img_url' : 'http://120.25.93.29/static/16.jpg'
                 }
             ]
         },
         {
             'tabId' : '7', 
             'name' : '公司法务外包', 
-            'tab_img_url' : 'https://o3pvuu23u.qnssl.com/img/lzwai.jpeg',
+            'tab_img_url' : 'http://120.25.93.29/static/1.jpg',
             'services' : 
             [
                 {
-                    'product_id' : '19', 
+                    'product_id' : '17', 
                     'product_name' : '创业公司专属法务（试用版）', 
                     'product_price' : '1000.0', 
                     'product_desc' : '职业律师诉讼服务', 
-                    'product_img_url' : 'https://o3pvuu23u.qnssl.com/img/lzwai.jpeg'
+                    'product_img_url' : 'http://120.25.93.29/static/17.jpg'
                 }, 
                 {
-                    'product_id' : '20', 
+                    'product_id' : '18', 
                     'product_name' : '创业公司专属法务（季度版）', 
                     'product_price' : '3000.0', 
                     'product_desc' : '细致入微，不会放过一个细节', 
-                    'product_img_url' : 'https://o3pvuu23u.qnssl.com/img/lzwai.jpeg'
+                    'product_img_url' : 'http://120.25.93.29/static/18.jpg'
                 },
                 {
-                    'product_id' : '21', 
+                    'product_id' : '19', 
                     'product_name' : '创业公司专属法务（年度版）', 
                     'product_price' : '6000.0', 
                     'product_desc' : '细致入微，不会放过一个细节', 
-                    'product_img_url' : 'https://o3pvuu23u.qnssl.com/img/lzwai.jpeg'
+                    'product_img_url' : 'http://120.25.93.29/static/19.jpg'
                 },
                 {
-                    'product_id' : '22', 
+                    'product_id' : '20', 
                     'product_name' : '公司专属法务（基础版）', 
                     'product_price' : '10000.0', 
                     'product_desc' : '细致入微，不会放过一个细节', 
-                    'product_img_url' : 'https://o3pvuu23u.qnssl.com/img/lzwai.jpeg'
+                    'product_img_url' : 'http://120.25.93.29/static/20.jpg'
                 },
                 {
-                    'product_id' : '23', 
+                    'product_id' : '21', 
                     'product_name' : '公司专属法务（升级版）', 
                     'product_price' : '20000.0', 
                     'product_desc' : '细致入微，不会放过一个细节', 
-                    'product_img_url' : 'https://o3pvuu23u.qnssl.com/img/lzwai.jpeg'
+                    'product_img_url' : 'http://120.25.93.29/static/21.jpg'
                 }
             ]
         }
