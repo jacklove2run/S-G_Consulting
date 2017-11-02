a102c05f164f67dd7a0f4c45008cf351b6067f89
jacklove2run <529842912@qq.com>
2017-11-03 02:36:37 +0800

    修改产品信息

--- testdb/testdb/Ylk/product_list.py
+++ testdb/testdb/Ylk/product_list.py
@@ -8,7 +8,6 @@ from django.http import HttpResponse
 from django.forms.models import model_to_dict 
 from ylawyer.models import ProductInfo, OrderList, UserInfo, SessionOpenId, SavedProductList, userAddrList
 from api_pub import current_datetime, check_session_value
-from product_list import PRODUCT_LIST
 import logging
 
      
