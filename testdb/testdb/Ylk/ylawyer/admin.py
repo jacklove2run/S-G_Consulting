d7e1513789eb2b47401f3ad9880d7404c5100c34
jacklove2run <529842912@qq.com>
2017-11-12 10:49:52 +0800

    修改后台

--- testdb/testdb/Ylk/ylawyer/admin.py
+++ testdb/testdb/Ylk/ylawyer/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from ylawyer import models
+from ylawyer.models import ProductInfo, OrderList, UserInfo, SessionOpenId, SavedProductList, userAddrList
+
 
 # Register your models here.
+
+
+class ProductInfoAdmin(admin.ModelAdmin):
+    list_display = ('product_id', 'product_name', 'product_price', 'product_desc', 'product_img_url', 'product_address', 'service_type', 'service_address', 'service_way', 'service_time')
+    # 定义搜索框以哪些字段可以搜索
+    search_fields = ('product_name', 'product_id')
+    
+class UserInfoAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'name', 'phone', 'email')
+    search_fields = ('name', 'phone', 'email')
+
+class OrderListAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'product_name', 'product_price', 'product_desc', 'time')
+    search_fields = ('user_id', 'product_name', 'time')
+# 引用的固定格式，注册的model和对应的Admin，Admin放在后边
+# 同样还有noregister方法：比如admin.site.noregister(Group)，把group这个表在admin中去掉（默认user和group都是注册到admin中的）
+#admin.site.register(models.UserProfile, UserProfileAdmin)
+admin.site.register(OrderList, OrderListAdmin)
+admin.site.register(ProductInfo, ProductInfoAdmin)
+
+admin.site.register(UserInfo, UserInfoAdmin)
